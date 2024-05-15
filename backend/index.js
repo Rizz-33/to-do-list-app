5a12a52f9e096c55a6ee47de1ff074746927eb54
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-05-15 13:46:51 +0530

    feat: Update todo

--- backend/index.js
+++ backend/index.js
@@ -23,14 +23,17 @@ const todos = [
   },
 ];
 
+//home
 app.get("/", (req, res) => {
   res.json({ msg: "To-Do List" });
 });
 
+//get todos
 app.get("/todos", (req, res) => {
   res.json(todos);
 });
 
+//get only one todo by id
 app.get("/todo/:id", (req, res) => {
   const todo = todos.find((todo) => todo.id === parseInt(req.params.id));
   if (!todo) {
@@ -39,6 +42,7 @@ app.get("/todo/:id", (req, res) => {
   res.json(todo);
 });
 
+//add todo
 app.post("/todos", (req, res) => {
   const todo = {
     id: todos.length + 1,
@@ -46,6 +50,17 @@ app.post("/todos", (req, res) => {
     completed: false,
   };
   todos.push(todo);
+  res.json(todo);
+});
+
+//update todo
+app.put("/todos/:id", (req, res) => {
+  const todo = todos.find((todo) => todo.id === parseInt(req.params.id));
+  if (!todo) {
+    return res.status(404).json({ msg: "Todo not found" });
+  }
+  todo.title = req.body.title;
+  todo.completed = req.body.completed;
   res.json(todo);
 });
 
