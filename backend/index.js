c04f13021bf20cce828666c5ccf3a87b97ff2f64
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-05-15 22:01:53 +0530

    feat: Db connection

--- backend/index.js
+++ backend/index.js
@@ -2,54 +2,45 @@ const express = require("express");
 const app = express();
 const uuid = require("uuid");
 const cors = require("cors");
+const mongoose = require("mongoose");
 
 const PORT = 3000;
+
+const connectionString =
+  "mongodb+srv://aarruwanthie:12$45$78@cluster0.ffhhoj8.mongodb.net/TodoList?retryWrites=true&w=majority&appName=Cluster0";
+
+mongoose
+  .connect(connectionString)
+  .then(() => console.log("Connected to DB"))
+  .catch((err) => console.log(err));
 
 app.use(express.json());
 app.use(cors());
 
 const todos = [
-  {
-    id: 1,
-    title: "Learn Express",
-    completed: true,
-  },
-  {
-    id: 2,
-    title: "Learn Golang",
-    completed: false,
-  },
-  {
-    id: 3,
-    title: "Learn   Django",
-    completed: true,
-  },
+  { id: 1, title: "Learn Express", completed: true },
+  { id: 2, title: "Learn Golang", completed: false },
+  { id: 3, title: "Learn Django", completed: true },
 ];
 
-//home
 app.get("/", (req, res) => {
   res.json({ msg: "To-Do List" });
 });
 
-//get todos
 app.get("/todos", (req, res) => {
   res.json(todos);
 });
 
-//get only one todo by id
 app.get("/todos/:id", (req, res) => {
   let todo = todos.filter((todo) => todo.id === parseInt(req.params.id));
   res.json({ data: todo });
 });
 
-//add todo
 app.post("/todos", (req, res) => {
-  console.log(req.body);
   todos.push({ id: uuid.v4(), ...req.body });
   res.json({ data: todos });
 });
 
-//update todo
 app.put("/todos/:id", (req, res) => {
   let todo = todos.find((todo) => todo.id === req.params.id);
   if (todo) {
@@ -61,7 +52,6 @@ app.put("/todos/:id", (req, res) => {
   }
 });
 
-//delete todo
 app.delete("/todos/:id", (req, res) => {
   let index = todos.findIndex((todo) => todo.id == req.params.id);
   todos.splice(index, 1);
